b9a8120082b556667b7fbcdba8f816c5e16b4d0e
NouxAR <arslanarda2381196@gmail.com>
2025-05-03 02:08:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -84,3 +84,8 @@ app.get('/api/dialog/:scene', async (req, res) => {
     res.status(500).send("MongoDB'den diyalog alınamadı.");
   }
 });
+
+app.get('/api/dialog', async (req, res) => {
+  const allDialogs = await Dialog.find({});
+  res.json(allDialogs);
+});
