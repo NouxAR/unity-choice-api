551504f742bb5cff84f5e266a212389e63a4c25e
NouxAR <arslanarda2381196@gmail.com>
2025-06-10 23:09:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const db = mongoose.connection;
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const singleChoiceSchema = new mongoose.Schema({
   npc: String,
